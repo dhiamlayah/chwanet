7a3525f451f1e213b7ae10949c0d74a1b8cd2d7b
dhiamlayah <113603828+dhiamlayah@users.noreply.github.com>
2023-10-08 22:16:46 +0000

    part two in profiles

---
+++ client/src/components/profileWorker/WorkerDiscreption.tsx
@@ -0,0 +1,55 @@
+const WorkerDiscreption = ({
+  discreption,
+  phone,
+  state,
+  delegation,
+  workName,
+  isMe,
+}: any) => {
+  console.log(isMe);
+
+  return (
+    <div
+      id="workerData"
+      className="pt-3 text-end"
+      style={{ backgroundColor: "#rgb(255 224 217 / 28%)" }}
+    >
+      <div id="photo" className="m-1 p-2">
+        <div className="  p-8 ">
+          <div className={isMe ? "d-flex justify-content-between" : ""}>
+            {isMe && <p className="p-2">X</p>}
+            <p className="fw-bold">{workName}</p>
+          </div>
+          <p>{discreption}</p>
+         </div>
+      </div>
+      <div className="  m-1 " id="cartId">
+        <div className={isMe ? "d-flex justify-content-between" : ""}>
+          {isMe && <p className="">X</p>}
+          <p>
+            <span className="fw-bold">هاتف : </span>
+            {phone}
+          </p>
+        </div>
+        <div className={isMe ? "d-flex justify-content-between" : ""}>
+          {isMe && <p className="">X</p>}
+          <p>
+            <span className="fw-bold">ولاية : </span>
+            {state}
+          </p>
+        </div> 
+        <div className={isMe ? "d-flex justify-content-between" : ""}>
+        {isMe && <p className="">X</p>} 
+            <p>
+              <span className="fw-bold">مدينة :</span>
+              {delegation}
+            </p>
+        
+        </div>
+      </div>
+   
+    </div>
+  );
+};
+
+export default WorkerDiscreption;
---
+++ client/src/components/profileWorker/WorkerProfil.tsx
@@ -0,0 +1,158 @@
+ 
+ import {useState,useEffect} from 'react'
+ import "../../StyleDesign/meAsWorker.css";
+import WorkerDiscreption from './WorkerDiscreption';
+import axios from 'axios';
+
+ const WorkerProfil = ({profilPicture,WorkerInformations}:any) => {
+   const url: string = process.env.REACT_APP_port + "/meAs";
+   const [number, setNumber] = useState(1);
+   const [user,setUser]=useState<any>(null)
+   const [isMe,setIsMe]=useState<boolean>(false)
+   const {
+      firstName,
+      lastName,
+      discreption,
+      phone,
+      state,
+      delegation,
+      workName,
+    } = WorkerInformations;
+
+    const getCurrentUser = async () => {
+      const newUrl = url + localStorage.getItem("User");
+      try {
+        await axios
+          .get(newUrl, {
+            headers: {
+              token: localStorage.getItem("Token"),
+            },
+          })
+          .then((res) => {
+            console.log("successfuly", res.data);
+            setUser(res.data.user);
+          });
+      } catch (err) {
+        console.log("ther is an error to get current user ", err);
+      }
+    };
+
+    const chooseImg = () => {
+      const imgUrl = `url('/images/home/builder.jpg')`;
+      return imgUrl;
+    };
+    const handleClick = (numb: number) => {
+      if (numb === 1) {
+        setNumber(1);
+      } else if (numb === 2) {
+        setNumber(2);
+      } else {
+        setNumber(3);
+      }
+    };
+
+    useEffect(() => {
+      const token = localStorage.getItem("Token");
+      if (token) {
+        getCurrentUser();
+      }
+ 
+    }, []);
+
+    useEffect(()=>{
+      if(user){
+         isThisMe()
+      }
+    },[user])
+
+    const isThisMe = ()=>{
+      if(user){return setIsMe(user._id ===WorkerInformations._id)}
+      return setIsMe(false)
+    }
+    
+   return ( 
+      <div className="Profilbackground  ">
+      <div id="content">
+        <div
+          className="z-8 position-absolute"
+          style={{ backgroundImage: chooseImg() }}
+          id="couvertImg"
+         ></div>
+        <div className="z-5 position-absolute">
+          <div
+            className="z-8 position-absolute"
+            style={{ backgroundImage: `url('${profilPicture}')` }}
+            id="pdp"
+          ></div>
+        </div>
+
+        <div id="workerName" className='d-flex'>
+         {isMe && <h1 className="px-2">X</h1>} 
+          <h1 className="fw-bold ">
+            {firstName} {lastName}
+          </h1>
+        </div>
+
+        <div className="  list-group">
+          <hr
+            className="mx-5 d-flex justify-content-center "
+            style={{ width: "80%" }}
+          />
+
+          <div className="d-flex  justify-content-center ">
+            <button
+              type="button"
+              className={
+                number === 1
+                  ? "list-group-item list-group-item-action active mx-2 text-center"
+                  : "list-group-item list-group-item-action  text-center"
+              }
+              onClick={() => {
+                handleClick(1);
+              }}
+              style={{ width: "20%" }}
+            >
+             معلومات 
+            </button>
+            <button
+              type="button"
+              className={
+                number === 2
+                  ? "list-group-item list-group-item-action active mx-2 text-center"
+                  : "list-group-item list-group-item-action text-center "
+              }
+              onClick={() => {
+                handleClick(2);
+              }}
+              style={{ width: "20%" }}
+            >
+            الصور
+            </button>
+            <button
+              type="button"
+              className={
+                number === 3
+                  ? "list-group-item list-group-item-action active mx-2 text-center"
+                  : "list-group-item list-group-item-action text-center "
+              }
+              onClick={() => {
+                handleClick(3);
+              }}
+              style={{ width: "20%" }}
+            >
+              تعليقات
+            </button>
+          </div>
+        </div>
+        <hr
+            className="mx-5 d-flex justify-content-center "
+            style={{ width: "80%" }}
+          />
+      </div>
+
+      {number===1 &&  <WorkerDiscreption isMe={isMe} state={state} delegation={delegation} discreption={discreption} phone={phone} workName={workName}/>}
+       </div>    
+      );
+ }
+  
+ export default WorkerProfil;
